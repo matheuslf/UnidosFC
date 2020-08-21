45413bdfebeb1b4ab1d540812ebff3e2cfe6f941
Matheus <matheusleandroferreira@gmail.com>
2020-08-21 17:46:09 -0300

    Primeiro commit

--- app/src/main/java/unidos/programador/unidosfc/ui/login/LoginActivity.java
+++ app/src/main/java/unidos/programador/unidosfc/ui/login/LoginActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,38 +16,68 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import unidos.programador.unidosfc.MainActivity;
 import unidos.programador.unidosfc.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button novoUsuario, entrar;
+    private TextInputLayout usuario, senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        usuario = findViewById(R.id.usuario);
+        senha = findViewById(R.id.senha);
+
         entrar = findViewById(R.id.entrar);
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("usuario");
-                Query checkUser = reference.orderByChild("usuario").equalTo("matheuslf");
-                checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.exists()) {
 
+                // Limpa o erro do campo.
+                usuario.setError(null);
+                usuario.setErrorEnabled(false);
+                senha.setError(null);
+                senha.setErrorEnabled(false);
+
+                // Captura a informação digitada.
+                final String lsUsuario = usuario.getEditText().getText().toString();
+                final String lsSenha = senha.getEditText().getText().toString();
+
+                // Faz as validações.
+                if (lsUsuario.isEmpty()) {
+                    usuario.setError("Campo usuário obrigatório");
+                    usuario.setErrorEnabled(true);
+                }
+                else if (lsSenha.isEmpty()) {
+                    senha.setError("Campo senha obrigatório");
+                    senha.setErrorEnabled(true);
+                }
+                else {
+                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("usuarios");
+                    Query checkUser = reference.orderByChild("usuario").equalTo(lsUsuario);
+                    checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                final String senhaDB = dataSnapshot.child(usuario.getEditText().getText().toString()).child("senha").getValue(String.class);
+                                if (lsSenha.equals(senhaDB)) {
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                }
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        if (databaseError.getMessage() != null) {
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            if (databaseError.getMessage() != null) {
 
+                            }
                         }
-                    }
-                });
+                    });
+                }
             }
         });
 
--- app/src/main/java/unidos/programador/unidosfc/ui/login/RegistroActivity.java
+++ app/src/main/java/unidos/programador/unidosfc/ui/login/RegistroActivity.java
@@ -1,48 +1,105 @@
 package unidos.programador.unidosfc.ui.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
 
+import unidos.programador.unidosfc.MainActivity;
 import unidos.programador.unidosfc.R;
 import unidos.programador.unidosfc.database.UsuarioModel;
 
 public class RegistroActivity extends AppCompatActivity {
 
     private Button cadastrar;
+    private TextInputLayout nome;
+    private TextInputLayout usuario;
+    private TextInputLayout email;
+    private TextInputLayout telefone;
+    private TextInputLayout senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        nome = findViewById(R.id.nome);
+        usuario = findViewById(R.id.usuario);
+        email = findViewById(R.id.email);
+        telefone = findViewById(R.id.telefone);
+        senha = findViewById(R.id.senha);
+
         cadastrar = findViewById(R.id.cadastrar);
         cadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                FirebaseDatabase root = FirebaseDatabase.getInstance();
-                DatabaseReference reference = root.getReference("tb_usuarios");
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("usuarios");
+                Query checkUser = reference.orderByChild("usuarios").equalTo(usuario.getEditText().toString());
+                checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            new SweetAlertDialog(RegistroActivity.this, SweetAlertDialog.ERROR_TYPE)
+                                    .setTitleText("Erro")
+                                    .setContentText("Já existe um usuário cadastrado com este nome!")
+                                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                        @Override
+                                        public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                            sweetAlertDialog.dismissWithAnimation();
+                                        }
+                                    })
+                                    .show();
+                        } else {
 
-                UsuarioModel usuario = new UsuarioModel
-                                        (
-                                            48991645854L,
-                                            "Matheus Leandro Ferreira",
-                                            "matheuslf",
-                                            "matheusleandroferreira@gmail.com",
-                                            "bombom"
-                                        );
+                            // Faz o cadastro do usuário.
+                            FirebaseDatabase root = FirebaseDatabase.getInstance();
+                            DatabaseReference reference = root.getReference("usuarios");
+                            UsuarioModel user = new UsuarioModel
+                                    (
+                                            new Long(telefone.getEditText().getText().toString()),
+                                            nome.getEditText().getText().toString(),
+                                            usuario.getEditText().getText().toString(),
+                                            email.getEditText().getText().toString(),
+                                            senha.getEditText().getText().toString()
+                                    );
+                            reference.child(telefone.getEditText().getText().toString()).setValue(user);
 
-                reference.setValue(usuario);
+                            // Mostra a mensagem de sucesso.
+                            new SweetAlertDialog(RegistroActivity.this, SweetAlertDialog.SUCCESS_TYPE)
+                                    .setTitleText("Informação")
+                                    .setContentText("Usuário cadastrado !")
+                                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                        @Override
+                                        public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                            sweetAlertDialog.dismissWithAnimation();
+                                            finish();
+                                        }
+                                    })
+                                    .show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        if (databaseError.getMessage() != null) {
+
+                        }
+                    }
+                });
             }
         });
     }
